eb4b48e74eb8c8de21ef58a0b48d026ec694475e
Miguel Carranza <miguel@mindsnacks.com>
2014-03-04 11:31:37 -0800

    Initial commit. Very basic functionality.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Google Drive Downloader',
+      version='0.1',
+      description='Python utility to download documents in Google Drive',
+      author='Miguel Carranza',
+      author_email='miguel@miguelcarranza.es',
+      url='http://www.github.com/MiguelCarranza',
+      py_modules=['gdd'],
+      license='MIT License',
+      )
